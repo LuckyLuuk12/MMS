8d408c2b6922b087d9054335a825c7cc628f8a1a
Luuk Kablan <luckyluuk02@gmail.com>
2025-04-18 15:01:03 +0200

    feat: fix copy url to be encoded properly

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -9,7 +9,7 @@
   
   function handleCopyToClipboard() {
     if (selectedMeme) {
-      navigator.clipboard.writeText(`https://${location.hostname}/memes/${selectedMeme.name}`);
+      navigator.clipboard.writeText(`https://${location.hostname}/memes/${encodeURI(selectedMeme.name)}`);
       showNotification = true;
       setTimeout(() => {
         showNotification = false;
